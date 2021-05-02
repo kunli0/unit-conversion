5251eb8ae1332604ccf121001877c178019286b7
Adekunle Adelakun <kunle@xyrotech.com>
2021-05-02 18:20:38 -0400

    Fix

---
+++ src/Weight.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kunli0\UnitConversions;
+
+class Weight
+{
+
+    private float $kilograms;
+
+    public static function  fromKilograms(float $kilograms): self{
+        return new static($kilograms);
+    }
+
+    public function __construct(float $kilograms)
+    {
+
+        $this->kilograms = $kilograms;
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.204623;
+    }
+}
+
