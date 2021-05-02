eb4f4e5902701e562ed851a40f5651f374351b71
Adekunle Adelakun <kunle@xyrotech.com>
2021-05-02 18:06:48 -0400

    kilo to lbs

---
+++ tests/WeightTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kunli0\UnitConversions\Tests;
+
+use Kunli0\UnitConversions\Weight;
+use PHPUnit\Framework\TestCase;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_conver_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+
+        $this->assertEquals(220.4623, $lbs);
+
+    }
+}
