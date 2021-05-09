34078388fd96202de64f673445848b7a76e76fd7
kunli0 <kunli0@users.noreply.github.com>
2021-05-09 18:40:35 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -6,9 +6,8 @@ class Weight
 {
     private float $kilograms;
 
-    public static function fromKilograms(float $kilograms): self{
-
-
+    public static function fromKilograms(float $kilograms): self
+    {
         return new static($kilograms);
     }
 
